7968c2abec688bacb3e8f08073cae1ab5b38e9c9
Johnny Furchi <amanha888@gmail.com>
2021-03-22 21:20:07 +0000

    operações

---
+++ site-exemplo/script.js
@@ -0,0 +1,10 @@
+let spaceship = "Elemental"
+let velocity = 20
+
+// console.log(spaceship.length == 9 || velocity > 15)
+
+// console.log(velocity > 10 || velocity < 19)
+
+//console.log(velocity < 17 || spaceship == "Elemental")
+
+console.log(spaceship == "Golias" || velocity > 21)
